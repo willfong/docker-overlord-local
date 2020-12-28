86b5b1a48a50b4f3047c1bc6954101b2c18ba6c5
Will Fong <willfong@pm.me>
2020-12-28 19:43:27 +0800

    Initial commit

---
+++ api/db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+
+const agentSchema = new mongoose.Schema({
+	registration_end_date: Date,
+	estate_agent_license_no: String,
+	salesperson_name: String,
+	registration_no: String,
+	registration_start_date: Date,
+	estate_agent_name: String,
+	_id: Number
+});
+const Agent = mongoose.model('Agent', agentSchema);
+
+
+module.exports = {
+	Kitten,
+	Agent
+};
